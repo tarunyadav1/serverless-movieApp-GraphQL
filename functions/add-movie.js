298cefd6b9eca615e84547f38712d6df3ae68394
Tarun yadav <56935638+tarunyadav1@users.noreply.github.com>
2020-09-06 22:46:07 +0530

    added login

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -2,6 +2,23 @@ const { query } = require("./util/hasura");
 
 exports.handler = async (event, context) => {
   const { id, title, tagline, poster } = JSON.parse(event.body);
+  const { user } = context.clientContext;
+  const isLoggedIn = user && user.app_metadata && user.app_metadata.roles;
+  const roles = isLoggedIn ? user.app_metadata.roles : [];
+
+  if (!isLoggedIn || !roles.includes("admin")) {
+    return {
+      statusCode: 401,
+      body: "Unauthorized",
+    };
+  }
+
+  if (!id || !title || !tagline || !poster) {
+    return {
+      statusCode: 404,
+      body: "Empty Fields",
+    };
+  }
 
   const result = await query({
     query: `
--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,9 +1,10 @@
 module.exports = {
   // keep it short! shown in the header
-  title: 'Frontend Masters: Serverless with Jason Lengstorf',
+  title: "Serverless Movie App",
 
   // these are all optional and add links to the footer
-  repo: 'jlengstorf/frontendmasters-serverless',
+
+  repo: "tarunyadav1/serverless-movieApp-GraphQL",
   // episode:
   //   'https://www.learnwithjason.dev/creating-css-variable-font-text-effects',
   // tutorial: 'https://codepen.io/jlengstorf/pen/QWbdLjb',
